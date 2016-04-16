d77cdc9e2904f27e50a5d6145437ff94844ee883
Mads R. B. Kristensen <madsbk@gmail.com>
2016-04-16 23:15:24 +0200

    Fixed return BUG

--- lrcloud.py
+++ lrcloud.py
@@ -37,16 +37,17 @@ def unlock_file(filename):
 
 def copy_smart_previews(local_catalog, cloud_catalog, local2cloud=True):
     """Copy Smart Previews from local to cloud or
-       vica versa when 'local2cloud==False' """
+       vica versa when 'local2cloud==False'
+       NB: nothing happens if source dir doesn't exist"""
     
     lcat_noext = local_catalog[0:local_catalog.rfind(".lrcat")]
     ccat_noext = cloud_catalog[0:cloud_catalog.rfind(".lrcat")]
     lsmart = join(dirname(local_catalog),"%s Smart Previews.lrdata"%basename(lcat_noext))
     csmart = join(dirname(cloud_catalog),"%s Smart Previews.lrdata"%basename(ccat_noext))
-    if local2cloud:
+    if local2cloud and os.path.isdir(lsmart):
         logging.info("Copy Smart Previews - local to cloud: %s => %s"%(lsmart, csmart))
         distutils.dir_util.copy_tree(lsmart,csmart, update=1)
-    else:
+    elif os.path.isdir(csmart):
         logging.info("Copy Smart Previews - cloud to local: %s => %s"%(csmart, lsmart))
         distutils.dir_util.copy_tree(csmart,lsmart, update=1)
 
@@ -151,6 +152,7 @@ def parse_arguments():
         parser.error("No catalog exist! Either a local "\
                      "or a cloud catalog must exist")
         raise argparse.ArgumentError
+    return args
 
 
 if __name__ == "__main__":
