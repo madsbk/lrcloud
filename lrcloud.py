82fab4c064407802dff62bba393280f47a48b81b
Mads R. B. Kristensen <madsbk@gmail.com>
2016-05-05 22:48:49 +0200

    Implemented incremental catalog updates. Need major cleanup!

--- lrcloud.py
+++ lrcloud.py
@@ -7,7 +7,7 @@ import shutil
 import subprocess
 import logging
 import distutils.dir_util
-from os.path import join, basename, dirname, isfile
+from os.path import join, basename, dirname, isfile, abspath
 import traceback
 import zipfile
 from zipfile import ZIP_DEFLATED
@@ -15,6 +15,8 @@ import tempfile
 import hashlib
 from functools import partial
 from datetime import datetime
+import re
+import pprint
 
 DATETIME_FORMAT='%Y-%m-%d %H:%M:%S.%f'
 
@@ -101,6 +103,7 @@ def copy_catalog(local_catalog, cloud_catalog, local2cloud=True):
                 except OSError:
                     pass
                 shutil.move(tmpfile, dst)
+                print tmpfile, dst
             finally:
                 shutil.rmtree(tmpdir, ignore_errors=True)
     elif dzip:
@@ -108,11 +111,6 @@ def copy_catalog(local_catalog, cloud_catalog, local2cloud=True):
             z.write(src, arcname=basename(src))
     else:#None of them are zipped
         shutil.copy2(src, dst)
-
-
-def catalog2meta_file(catalog):
-    """Return the meta-data file name associated with 'catalog' """
-    return "%s.lrcloud.ini"%catalog
 
 
 def hashsum(filename):
@@ -127,6 +125,7 @@ def hashsum(filename):
 
 class MetaFile:
     """Representation of a meta-file"""
+
     def __init__(self, file_path):
         self.file_path = file_path
         config = cparser.ConfigParser()
@@ -145,6 +144,8 @@ class MetaFile:
                    value = t
                 except ValueError:
                     pass
+                except TypeError:
+                    pass
                 self._data[sec][name] = value
 
     def __getitem__(self, section):
@@ -162,6 +163,86 @@ class MetaFile:
         with open(self.file_path, 'w') as f:
             config.write(f)
 
+class Node:
+    def __init__(self, mfile):
+        self.mfile = mfile
+        self.parents = []
+        self.children = []
+        self.hash = mfile['changeset']['hash'] # Shortcut
+    def __repr__(self):
+        parents = "["
+        for parent in self.parents:
+            parents += "%s, "%parent.hash
+        parents += "]"
+        children = "["
+        for child in self.children:
+            children += "%s, "%child.hash
+        children += "]"
+        return "{%s, parents: %s, children: %s}"%(self.hash, parents, children)
+
+class ChangesetDAG:
+
+    def _get_all_cloud_mfiles(self, cloud_catalog):
+        ret = ["%s.lrcloud"%cloud_catalog]
+        cloud_dir = dirname(cloud_catalog)
+        for f in os.listdir(cloud_dir):
+            f = abspath(join(cloud_dir, f))
+            if not isfile(f):
+                continue
+            pattern = "%s_[0-9a-fA-F]+\.zip\.lrcloud"%cloud_catalog
+            if re.search(pattern, f):
+                ret.append(f)
+        return ret
+
+    def __init__(self, cloud_catalog):
+        self.nodes = {}  # Hash to node instance
+        self.leafs = []  # Leaf nodes
+        self.root = None # The root node
+
+        # Instantiate all nodes
+        for mfile in self._get_all_cloud_mfiles(cloud_catalog):
+            mfile = MetaFile(mfile) # File path to class instance
+            chash = mfile['changeset']['hash']
+            assert (chash not in self.nodes)
+            self.nodes[chash] = Node(mfile)
+        # Assign parents
+        for node in self.nodes.values():
+            if not node.mfile['changeset']['is_base']:
+                node.parents.append(self.nodes[node.mfile['parent']['hash']])
+                assert len(node.parents) == 1
+        # Assign children
+        for node in self.nodes.values():
+            for parent in node.parents:
+                self.nodes[parent.hash].children.append(node)
+                assert len(self.nodes[parent.hash].children) == 1
+        # Find leaf nodes
+        for node in self.nodes.values():
+            if len(node.children) == 0:
+                self.leafs.append(node)
+        # Find the root node, which is the base changeset
+        for node in self.nodes.values():
+            if node.mfile['changeset']['is_base']:
+                assert self.root is None
+                self.root = node
+
+        assert len(self.leafs) <= 1
+        assert self.root is not None
+
+    def path(self, a_hash, b_hash):
+        """Return nodes in the path between 'a' and 'b' going from
+        parent to child NOT including 'a' """
+
+        def _path(a, b):
+            if a is b:
+                return [a]
+            else:
+                assert len(a.children) == 1
+                return [a] + _path(a.children[0], b)
+
+        a = self.nodes[a_hash]
+        b = self.nodes[b_hash]
+        return _path(a, b)[1:]
+
 
 def cmd_init_push_to_cloud(args):
     """Initiate the local catalog and push it the cloud"""
@@ -174,7 +255,7 @@ def cmd_init_push_to_cloud(args):
     if isfile(ccat):
         args.error("[init-push-to-cloud] The cloud catalog already exist: %s"%ccat)
 
-    (lmeta, cmeta) = (catalog2meta_file(lcat), catalog2meta_file(ccat))
+    (lmeta, cmeta) = ("%s.lrcloud"%lcat, "%s.lrcloud"%ccat)
     if isfile(lmeta):
         args.error("[init-push-to-cloud] The local meta-data already exist: %s"%lmeta)
     if isfile(cmeta):
@@ -185,21 +266,25 @@ def cmd_init_push_to_cloud(args):
     if not lock_file(lcat):
         raise RuntimeError("The catalog %s is locked!"%lcat)
 
-    #Let's "lock" the cloud catalog
-    logging.info("Locking cloud catalog: %s"%(ccat))
-    if not lock_file(ccat):
-        raise RuntimeError("The cloud catalog %s is locked!"%ccat)
+    #Copy catalog from local to cloud, which becomes the new "base" changeset
+    copy_catalog(lcat, ccat, local2cloud=True)
 
     # Write meta-data both to local and cloud
     mfile = MetaFile(lmeta)
-    mfile['master']['hash'] = hashsum(lcat)
-    mfile['master']['modification_utc'] = datetime.utcnow().strftime(DATETIME_FORMAT)[:-4]
+    utcnow = datetime.utcnow().strftime(DATETIME_FORMAT)[:-4]
+    mfile['catalog']['hash'] = hashsum(lcat)
+    mfile['catalog']['modification_utc'] = utcnow
+    mfile['catalog']['filename'] = lcat
+    mfile['last_push']['filename'] = ccat
+    mfile['last_push']['hash'] = hashsum(lcat)
+    mfile['last_push']['modification_utc'] = utcnow
     mfile.flush()
-    logging.info("Copying local meta-data to cloud: %s => %s"%(lmeta, cmeta))
-    shutil.copy2(lmeta, cmeta)
-
-    #Copy catalog from local to cloud
-    copy_catalog(lcat, ccat, local2cloud=True)
+    mfile = MetaFile(cmeta)
+    mfile['changeset']['is_base'] = True
+    mfile['changeset']['hash'] = hashsum(lcat)
+    mfile['changeset']['modification_utc'] = utcnow
+    mfile['changeset']['filename'] = ccat
+    mfile.flush()
 
     #Let's copy Smart Previews
     if not args.no_smart_previews:
@@ -208,8 +293,6 @@ def cmd_init_push_to_cloud(args):
     #Finally,let's unlock the catalog files
     logging.info("Unlocking local catalog: %s"%(lcat))
     unlock_file(lcat)
-    logging.info("Unlocking cloud catalog: %s"%(ccat))
-    unlock_file(ccat)
 
     logging.info("[init-push-to-cloud]: Success!")
 
@@ -225,7 +308,7 @@ def cmd_init_pull_from_cloud(args):
     if not isfile(ccat):
         args.error("[init-pull-from-cloud] The cloud catalog does not exist: %s"%ccat)
 
-    (lmeta, cmeta) = (catalog2meta_file(lcat), catalog2meta_file(ccat))
+    (lmeta, cmeta) = ("%s.lrcloud"%lcat, "%s.lrcloud"%ccat)
     if isfile(lmeta):
         args.error("[init-pull-from-cloud] The local meta-data already exist: %s"%lmeta)
     if not isfile(cmeta):
@@ -236,15 +319,37 @@ def cmd_init_pull_from_cloud(args):
     if not lock_file(lcat):
         raise RuntimeError("The catalog %s is locked!"%lcat)
 
-    #Let's "lock" the cloud catalog
-    logging.info("Locking cloud catalog: %s"%(ccat))
-    if not lock_file(ccat):
-        raise RuntimeError("The cloud catalog %s is locked!"%ccat)
-
-    #Copy from cloud to local
+    #Copy base from cloud to local
     copy_catalog(lcat, ccat, local2cloud=False)
-    logging.info("Copying cloud meta-data to local: %s => %s"%(cmeta, lmeta))
-    shutil.copy2(cmeta, lmeta)
+
+    #Apply changesets
+    cloudDAG = ChangesetDAG(ccat)
+    path = cloudDAG.path(cloudDAG.root.hash, cloudDAG.leafs[0].hash)
+    for node in path:
+        try:
+            os.remove("/tmp/tmp.patch")
+        except OSError:
+            pass
+        copy_catalog(node.mfile['changeset']['filename'], "/tmp/tmp.patch", local2cloud=True)
+        print "mv %s %s"%(lcat, "/tmp/tmp.lcat")
+        shutil.move(lcat, "/tmp/tmp.lcat")
+
+        cmd = args.patch_cmd.replace("$in1", "/tmp/tmp.lcat")\
+                            .replace("$patch", "/tmp/tmp.patch")\
+                            .replace("$out", lcat)
+        logging.info("Patch: %s"%cmd)
+        subprocess.call(cmd, shell=True)
+
+    # Write meta-data both to local and cloud
+    mfile = MetaFile(lmeta)
+    utcnow = datetime.utcnow().strftime(DATETIME_FORMAT)[:-4]
+    mfile['catalog']['hash'] = hashsum(lcat)
+    mfile['catalog']['modification_utc'] = utcnow
+    mfile['catalog']['filename'] = lcat
+    mfile['last_push']['filename'] = cloudDAG.leafs[0].mfile['changeset']['filename']
+    mfile['last_push']['hash'] = cloudDAG.leafs[0].mfile['changeset']['hash']
+    mfile['last_push']['modification_utc'] = cloudDAG.leafs[0].mfile['changeset']['modification_utc']
+    mfile.flush()
 
     #Let's copy Smart Previews
     if not args.no_smart_previews:
@@ -253,8 +358,6 @@ def cmd_init_pull_from_cloud(args):
     #Finally, let's unlock the catalog files
     logging.info("Unlocking local catalog: %s"%(lcat))
     unlock_file(lcat)
-    logging.info("Unlocking cloud catalog: %s"%(ccat))
-    unlock_file(ccat)
 
     logging.info("[init-pull-from-cloud]: Success!")
 
@@ -267,7 +370,7 @@ def cmd_normal(args):
     """
 
     (lcat, ccat) = (args.local_catalog, args.cloud_catalog)
-    (lmeta, cmeta) = (catalog2meta_file(lcat), catalog2meta_file(ccat))
+    (lmeta, cmeta) = ("%s.lrcloud"%lcat, "%s.lrcloud"%ccat)
 
     if not isfile(lcat):
         args.error("The local catalog does not exist: %s"%lcat)
@@ -279,60 +382,90 @@ def cmd_normal(args):
     if not lock_file(lcat):
         raise RuntimeError("The catalog %s is locked!"%lcat)
 
-    #Let's "lock" the cloud catalog
-    logging.info("Locking cloud catalog: %s"%(ccat))
-    if not lock_file(ccat):
-        raise RuntimeError("The cloud catalog %s is locked!"%ccat)
+    #Backup the local catalog (overwriting old backup)
+    try:
+        logging.info("Removed old backup: %s.backup"%lcat)
+        os.remove("%s.backup"%lcat)
+    except OSError:
+        pass
+    logging.info("Backup: %s => %s.backup"%(lcat, lcat))
+    shutil.copy(lcat, "%s.backup"%lcat)
 
     lmfile = MetaFile(lmeta)
     cmfile = MetaFile(cmeta)
 
-    if lmfile['master']['hash'] != cmfile['master']['hash']:
-        logging.info("The local catalog needs updating")
-
-        #Make sure we don't overwrite a modified local catalog
-        if lmfile['master']['modification_utc'] > cmfile['master']['modification_utc']:
-            raise RuntimeError("The local catalog is newer than the cloud catalog. "
-                               "Please remove one of them: '%s' or '%s'"%(lcat,ccat))
-
-        #Backup the local catalog (overwriting old backup)
+    #Apply changesets
+    cloudDAG = ChangesetDAG(ccat)
+    path = cloudDAG.path(lmfile['last_push']['hash'], cloudDAG.leafs[0].hash)
+    for node in path:
         try:
-            logging.info("Removed old backup: %s.backup"%lcat)
-            os.remove("%s.backup"%lcat)
+            os.remove("/tmp/tmp.patch")
         except OSError:
             pass
-        logging.info("Backup: %s => %s.backup"%(lcat, lcat))
-        shutil.move(lcat, "%s.backup"%lcat)
+        copy_catalog(node.mfile['changeset']['filename'], "/tmp/tmp.patch", local2cloud=True)
+        print "mv %s %s"%(lcat, "/tmp/tmp.lcat")
+        shutil.move(lcat, "/tmp/tmp.lcat")
 
-        #Copy from cloud to local
-        copy_catalog(lcat, ccat, local2cloud=False)
-    else:
-        logging.info("The local catalog is up to date")
+        cmd = args.patch_cmd.replace("$in1", "/tmp/tmp.lcat")\
+                            .replace("$patch", "/tmp/tmp.patch")\
+                            .replace("$out", lcat)
+        logging.info("Patch: %s"%cmd)
+        subprocess.call(cmd, shell=True)
 
     #Let's copy Smart Previews
     if not args.no_smart_previews:
         copy_smart_previews(lcat, ccat, local2cloud=False)
 
-    #Let's unlock the local catalog so that Lightrome can read it
+    #Backup the local catalog (overwriting old backup)
+    try:
+        logging.info("Removed old backup: %s.backup"%lcat)
+        os.remove("%s.backup"%lcat)
+    except OSError:
+        pass
+    logging.info("Backup: %s => %s.backup"%(lcat, lcat))
+    shutil.copy(lcat, "%s.backup"%lcat)
+
+    #Let's unlock the local catalog so that Lightroom can read it
     logging.info("Unlocking local catalog: %s"%(lcat))
     unlock_file(lcat)
 
     #Now we can start Lightroom
     logging.info("Starting Lightroom: %s %s"%(args.lightroom_exec, lcat))
-    if args.lightroom_exec is not None:
-        subprocess.call([args.lightroom_exec, lcat])
+    if args.lightroom_exec is not None or True:
+        #subprocess.call(args.lightroom_exec, lcat)
+        subprocess.call("echo hej >> %s"%lcat, shell=True)
     logging.info("Lightroom exit")
 
-    # Write meta-data both to local and cloud
-    mfile = MetaFile(lmeta)
-    mfile['master']['hash'] = hashsum(lcat)
-    mfile['master']['modification_utc'] = datetime.utcnow().strftime(DATETIME_FORMAT)[:-4]
-    mfile.flush()
-    logging.info("Copying local meta-data to cloud: %s => %s"%(lmeta, cmeta))
-    shutil.copy2(lmeta, cmeta)
+    args.diff_cmd = args.diff_cmd.replace("$in1", "%s.backup"%lcat)\
+                                 .replace("$in2", lcat)\
+                                 .replace("$out", "/tmp/tmp.patch")
+    logging.info("Diff: %s"%args.diff_cmd)
+    subprocess.call(args.diff_cmd, shell=True)
 
-    #Copy from local to cloud
-    copy_catalog(lcat, ccat, local2cloud=True)
+    patch = "%s_%s.zip"%(ccat, hashsum("/tmp/tmp.patch"))
+    copy_catalog("/tmp/tmp.patch", patch, local2cloud=True)
+
+    # Write cloud meta-data
+    mfile = MetaFile("%s.lrcloud"%patch)
+    utcnow = datetime.utcnow().strftime(DATETIME_FORMAT)[:-4]
+    mfile['changeset']['is_base'] = False
+    mfile['changeset']['hash'] = hashsum("/tmp/tmp.patch")
+    mfile['changeset']['modification_utc'] = utcnow
+    mfile['changeset']['filename'] = patch
+    mfile['parent']['is_base']          = cloudDAG.leafs[0].mfile['changeset']['is_base']
+    mfile['parent']['hash']             = cloudDAG.leafs[0].mfile['changeset']['hash']
+    mfile['parent']['modification_utc'] = cloudDAG.leafs[0].mfile['changeset']['modification_utc']
+    mfile['parent']['filename']         = cloudDAG.leafs[0].mfile['changeset']['filename']
+    mfile.flush()
+
+    # Write local meta-data
+    mfile = MetaFile(lmeta)
+    mfile['catalog']['hash'] = hashsum(lcat)
+    mfile['catalog']['modification_utc'] = utcnow
+    mfile['last_push']['filename'] = patch
+    mfile['last_push']['hash'] = hashsum("/tmp/tmp.patch")
+    mfile['last_push']['modification_utc'] = utcnow
+    mfile.flush()
 
     #Let's copy Smart Previews
     if not args.no_smart_previews:
@@ -341,8 +474,6 @@ def cmd_normal(args):
     #Finally, let's unlock the catalog files
     logging.info("Unlocking local catalog: %s"%(lcat))
     unlock_file(lcat)
-    logging.info("Unlocking cloud catalog: %s"%(ccat))
-    unlock_file(ccat)
 
 
 def read_config_file(args):
@@ -446,6 +577,22 @@ def parse_arguments():
         type=str,
         default=default_config_path()
     )
+    parser.add_argument(
+        '--diff-cmd',
+        help="The command that given two files, $in1 and $in2, "
+             "produces a diff file $out",
+        type=str,
+        #default="./jdiff -f $in1 $in2 $out"
+        default="bsdiff $in1 $in2 $out"
+    )
+    parser.add_argument(
+        '--patch-cmd',
+        help="The command that given a file, $in1, and a path, "
+             "$patch, produces a file $out",
+        type=str,
+        #default="./jptch $in1 $patch $out"
+        default="bspatch $in1 $out $patch"
+    )
     args = parser.parse_args()
     args.error = parser.error
 
@@ -475,7 +622,6 @@ if __name__ == "__main__":
             cmd_normal(args)
     finally:
         unlock_file(args.local_catalog)
-        unlock_file(args.cloud_catalog)
 
     write_config_file(args)
 
